63b3e66e9782dcde0aa4c85a2d0d28bb43d9ccbf
Jonathan Weyermann <weyermannx@gmail.com>
2015-04-24 21:31:23 -0600

    Basic PHP blog with Admin Area

---
+++ admin/includes/footer.php
@@ -0,0 +1,23 @@
+		
+		</div><!-- /.blog-main -->
+
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>PHPLoversblog &copy; 2015</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../js/bootstrap.js"></script>
+  </body>
+</html>
